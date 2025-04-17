8e8ae18dd051da395e85cd95f713edde56c0aca2
Olti-s <sogojevaolti451@gmail.com>
2025-04-17 17:44:38 +0200

    .........

--- index.php
+++ index.php
@@ -3,3 +3,38 @@
 
 
 <?php ?>
+
+<h1 id="topw" >New Literature Posts</h1>
+<div id="box">
+<div  class="card" style="width: 19rem;" >
+  <img src="<?php echo get_template_directory_uri();  ?>/img/doors.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Echo of the old books</h5>
+    <p class="card-text">Rare-book dealer Ashlyn Greer’s affinity for books extends beyond the intoxicating scent of old paper, ink, and leather. She can feel the echoes of the books’ previous owners—an emotional fingerprint only she can read. When Ashlyn discovers a pair of beautifully bound volumes that appear to have never been published, her gift quickly becomes an obsession. </p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+<div class="card" style="width: 18rem;">
+  <img src="<?php echo get_template_directory_uri();  ?>p1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Echo of the old books</h5>
+    
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+
+
